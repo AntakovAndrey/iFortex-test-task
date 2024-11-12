edca2d20914891be68e7e5b9d5e99382c7d3f713
AntakovAndrey <andrey.antakov05@gmail.com>
2024-11-12 22:01:09 +0300

    Order service added not implemented

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,11 @@
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService:IOrderService{
+        public Task<Order> GetOrder()=>throw new NotImplementedException();
+
+        public Task<List<Order>> GetOrders() => throw new NotImplementedException();
+    }
+}
