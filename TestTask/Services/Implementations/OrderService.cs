abaff54b6bf21045469478556516b4d88cd8d3fe
AntakovAndrey <andrey.antakov05@gmail.com>
2024-11-14 00:12:54 +0300

    orderService implemented

--- TestTask/Services/Implementations/OrderService.cs
+++ TestTask/Services/Implementations/OrderService.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
 using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations
 {
     public class OrderService:IOrderService{
-        public Task<Order> GetOrder()=>throw new NotImplementedException();
+        private readonly ApplicationDbContext _appDbContext;
+        public OrderService(ApplicationDbContext applicationDbContext)
+        {
+            this._appDbContext = applicationDbContext;
+        }
+        public Task<Order> GetOrder()
+        {
+            return _appDbContext.Orders.Where(o=>o.Quantity>1).OrderByDescending(o=>o.CreatedAt).FirstAsync();
+        }
 
-        public Task<List<Order>> GetOrders() => throw new NotImplementedException();
+        public Task<List<Order>> GetOrders()
+        {
+            return _appDbContext.Orders.Where(o=>o.User.Status==UserStatus.Active).OrderBy(o=>o.CreatedAt).ToListAsync();
+        }
     }
 }
