395a7aed98c5a7b3f23d8259a2a250ab3637c5cf
AntakovAndrey <andrey.antakov05@gmail.com>
2024-11-14 00:13:38 +0300

    userService implemented

--- TestTask/Services/Implementations/UserService.cs
+++ TestTask/Services/Implementations/UserService.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
 using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations
 {
     public class UserService:IUserService{
-        public Task<User> GetUser() => throw new NotImplementedException();
+        private readonly ApplicationDbContext _appDbContext;
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            this._appDbContext = applicationDbContext;
+        }
+        public Task<User> GetUser()
+        {
+           return _appDbContext.Orders.Where(o=>o.CreatedAt.Year==2003&&o.Status==OrderStatus.Delivered).OrderByDescending(o=>o.Quantity).Select(o=>o.User).FirstAsync();
+        } 
 
-        public Task<List<User>> GetUsers() => throw new NotImplementedException();
+        public Task<List<User>> GetUsers()
+        {
+            return _appDbContext.Orders.Where(o=>o.CreatedAt.Year==2010&&o.Status==OrderStatus.Paid).Select(o=>o.User).ToListAsync();
+        }
     }
 }
